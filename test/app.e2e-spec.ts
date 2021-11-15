24c3c483ef7848e6d259c31e3792f793bb708b2d
홍대현 <fholng@outlook.com>
2021-11-15 17:51:03 +0900

    nest e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,19 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello, World!');
+  });
+  describe(`/movies`, () => {
+    it(`GET`, () => {
+      return request(app.getHttpServer()).get(`/movies`).expect(200).expect([]);
+    });
+    it(`POST`, () => {
+      return request(app.getHttpServer()).post(`/movies`).send(
+        { title: `Test`, year: 2020, genres: [`test`] }
+      ).expect(201);
+    });
+    it(`DELETE`, () => {
+      return request(app.getHttpServer()).delete(`/movies`).expect(404);
+    });
   });
 });
